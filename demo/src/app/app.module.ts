89c1b1faef5f2180c8b39dca4a1b6e96fd884721
Anthony Nahas <anthony.na@hotmail.de>
2019-03-16 16:18:41 +0100

    fix(demo): updated the demo app

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,32 +1,34 @@
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {TransferHttpCacheModule} from '@nguniversal/common';
 
-import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { TransferHttpCacheModule } from '@nguniversal/common';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppSharedModule } from './shared/shared.module';
-import { HomeModule } from './home/home.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppSharedModule} from './shared/shared.module';
+import {HomeModule} from './home/home.module';
+import {AppComponent} from './app.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {MarkdownModule} from 'ngx-markdown';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        // Add .withServerTransition() to support Universal rendering.
-        // The application ID can be any identifier which is unique on
-        // the page.
-        BrowserModule.withServerTransition({ appId: 'ngx-animated-gradient-demo-id' }),
-        TransferHttpCacheModule,
-        FormsModule,
-        HttpModule,
-        AppRoutingModule,
-        AppSharedModule,
-        HomeModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    // Add .withServerTransition() to support Universal rendering.
+    // The application ID can be any identifier which is unique on
+    // the page.
+    BrowserModule.withServerTransition({appId: 'ngx-animated-gradient-demo-id'}),
+    MarkdownModule.forRoot({loader: HttpClient}),
+    TransferHttpCacheModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    AppSharedModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- demo/src/app/home/home.module.ts
+++ demo/src/app/home/home.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {  NgxAnimatedGradientModule  } from 'ngx-animated-gradient';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NgxAnimatedGradientModule} from 'ngx-animated-gradient';
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
+import {HomeRoutingModule} from './home-routing.module';
+import {HomeComponent} from './home.component';
+import {MarkdownModule} from 'ngx-markdown';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        NgxAnimatedGradientModule,
-        HomeRoutingModule,
-    ],
-    declarations: [HomeComponent],
+  imports: [
+    CommonModule,
+    NgxAnimatedGradientModule,
+    MarkdownModule.forChild(),
+    HomeRoutingModule,
+  ],
+  declarations: [HomeComponent],
 })
-export class HomeModule { }
+export class HomeModule {
+}
