ce60d19eb72c9fc5d6413d29b7995545f4856675
Anthony Nahas <anthony.na@hotmail.de>
2019-03-18 00:23:17 +0100

    chore(demo): :rocket: deploy new version

--- assets/md/example.md
+++ assets/md/example.md
@@ -2,14 +2,13 @@
 <div ngxAnimatedGradient></div>
 ```
 
-another full example 
+another full example
 
 ```html
 <div class="card">
-      <div class="card-header">
-        ngx-animated-gradient
-      </div>
-      <div class="card-body" ngxAnimatedGradient style="height: 400px">
-      </div>
-    </div>
+  <div class="card-header">
+    ngx-animated-gradient
+  </div>
+  <div class="card-body" ngxAnimatedGradient [gradientSpeed]="0.002" [tickSpeed]="16" style="height: 400px"></div>
+</div>
 ```
--- doc/js/menu-wc.js
+++ doc/js/menu-wc.js
@@ -90,7 +90,13 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                         </div>
                         <ul class="links collapse" ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
                             <li class="link">
+                                <a href="miscellaneous/enumerations.html" data-type="entity-link">Enums</a>
+                            </li>
+                            <li class="link">
                                 <a href="miscellaneous/functions.html" data-type="entity-link">Functions</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/typealiases.html" data-type="entity-link">Type aliases</a>
                             </li>
                         </ul>
                     </li>
